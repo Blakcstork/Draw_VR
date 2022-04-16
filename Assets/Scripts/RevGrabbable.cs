728f5197b712ab180d096ad09c71441f293090bc
이종상 <helius96@naver.com>
2022-04-16 22:35:34 +0900

    alone

--- Assets/Scripts/RevGrabbable.cs
+++ Assets/Scripts/RevGrabbable.cs
@@ -1,9 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class RevGrabbable : MonoBehaviour
 {
+
+    public float grabDistance = 1;
+    public float grabFlyTime = 2f;
+    public bool shouldFly = true;
+
+    [HideInInspector]
+    public bool inHand = false;
+
+    private RevOutline outlineComponent;
+
+    private RevControllerInput input;
+    private float grabStartTime;
+    private Vector3 grabStartPosition;
+
+    private float kickStartTime;
+    private float maxKickDuration = 1f;
+    private float minKickAngle = .25f;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
